ebb30758f8e398547f136f1446fd6d5a83f36067
tuyiz <honore.tuyizere73@gmail.com>
2023-02-16 09:59:21 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - A program that prints line of wards using printf function
+ * return: 0 - when the program is success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
