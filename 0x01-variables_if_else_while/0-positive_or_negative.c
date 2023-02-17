b737c5aa8954a5eb23c2619b8e5d09c7ed4d58d3
tuyiz <honore.tuyizere73@gmail.com>
2023-02-17 10:34:36 -0800

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -9,18 +9,17 @@
  * return: Always (Success)
  */
 int main(void)
-{       
+{
         int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
-                
-                if ( n > 0)
+	
+	if (n > 0)
 		{
 			printf("%d is positve\n ", n);
 		}
-		else if ( n == 0)
+		else if (n == 0)
 		{
 			printf("%d is zero\n ", n);
 		}
