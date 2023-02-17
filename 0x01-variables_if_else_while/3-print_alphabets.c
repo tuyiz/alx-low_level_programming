7b899788670f3a6de224c9f6a02db507960a602a
tuyiz <honore.tuyizere73@gmail.com>
2023-02-17 13:21:30 -0800

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+/**
+ * main - A pragam that print alphabet in upper and lower cases
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+        char c;
+
+        for (c = 'a'; c <= 'z'; c++)
+        putchar(c);
+
+	for (c = 'A'; c <= 'Z'; c++)
+	putchar(c);
+        putchar('\n');
+        return (0);
+}
