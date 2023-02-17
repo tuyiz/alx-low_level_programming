72549b5bfcef3d3f66155b57d939a3d85a0d5919
tuyiz <honore.tuyizere73@gmail.com>
2023-02-17 13:51:16 -0800

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - A program that print single digit of base 10
+ * return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d \n", i);
+	}
+	return (0);
+}
