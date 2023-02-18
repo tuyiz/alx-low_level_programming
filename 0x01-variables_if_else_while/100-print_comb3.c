8e4d4608d1cff929e2e07313041ee3885d08b390
tuyiz <honore.tuyizere73@gmail.com>
2023-02-18 03:12:02 -0800

    100-print_comb3.c

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - A program that print all passiple combination of two digit
+ * return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i,d;
+
+	for (i = '0'; i < '9'; i++)
+	{
+		for (d = '1'; d <= '0'; d++)
+		{
+		if (d != i)
+		{
+		putchar(i);
+		putchar(d);
+		if (i == '8' && '9')
+		continue;
+		putchar(',');
+		putchar(' ');
+		}
+		}
+	}
+	putchar('\n');
+	putchar(' ');
+
+	return (0);
+}
