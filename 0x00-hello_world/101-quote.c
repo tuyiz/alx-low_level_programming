b7a6730926e54e6def9bee9a359202f84e674315
tuyiz <honore.tuyizere73@gmail.com>
2023-02-20 08:34:58 -0800

    5-printf.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include<unistd.h>
+/** main - "print and that piece of art is useful" - Dora Korpar, 2015-10-19,
+ * flowed by a newline, to standard error
+ * return: always 1
+ */
+
+int maim(void)
+{
+	write(2, "and that piece of art is useful" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
