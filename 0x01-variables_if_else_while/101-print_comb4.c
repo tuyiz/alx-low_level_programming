5b0f068b88aa9d504f5272dfe6311886dd13c575
tuyiz <honore.tuyizere73@gmail.com>
2023-02-20 10:48:44 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,18 +1,18 @@
 #include<stdio.h>
 /**
  * main - A progam that print all possible combination of three digts
- * return: Always 0 (Success)
+ * return: -  Always 0 (Success)
  */
 
 int main(void)
 {
-	int i,d,p;
+	int i, d, p;
 
 	for (i = '0'; i < '8'; i++)
 	{
-	for (d = i+1; d < '9'; d++)
+	for (d = i + 1; d < '9'; d++)
 	{
-	for (p = d+1; p <= '9'; p++)
+	for (p = d + 1; p <= '9'; p++)
 	{
 	putchar(i);
 	putchar(d);
