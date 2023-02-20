1314905e79e16cfec1cb214eabe9f286893e5fc9
tuyiz <honore.tuyizere73@gmail.com>
2023-02-20 08:52:12 -0800

    101-quote.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@
  */
 int main(void)
 {
-	printf("size of char:%c byte(s)\n"; sizeof(char));
-	printf("size of int:%d byte(S)\n", sizeof(int));
-	printf("size of long:%ld byte(s)\n", sizeof(long int));
-	printf("size of long long int:%lld bbyte(s)\n", sizeof(long long int));
-	printf("size of float:%f bytes(s)\n", sizeof(float));
+	printf("size of char:%zu byte(s)\n"; sizeof(char));
+	printf("size of int:%zu byte(S)\n", sizeof(int));
+	printf("size of long:%zu byte(s)\n", sizeof(long int));
+	printf("size of long long int:%zu bbyte(s)\n", sizeof(long long int));
+	printf("size of float:%zu bytes(s)\n", sizeof(float));
 	return (0);
 }
