53a2cb8705f84e1ffbe2f9741aa5b1f1095e6b9c
tuyiz <honore.tuyizere73@gmail.com>
2023-02-21 13:26:02 -0800

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,7 +1,8 @@
 #include"main.h"
 /**
  * _abs print absolute value of a number
- * return: Absolute value of a number or 0
+ * @c: the number to be computed
+ * return: Absolute value of a number or zero
  */
 
 int _abs(int c)
@@ -9,6 +10,7 @@ int _abs(int c)
 	if (c < 0)
 	{
 	int abs_val;
+
 	abs_val = c * -1;
 	return (abs_val);
 	}
