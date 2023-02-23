f68fb4dd757fb3e1cb946cd4602fb89493bbd32c
tuyiz <honore.tuyizere73@gmail.com>
2023-02-23 02:03:42 -0800

    0-main.c, main.h

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * main - test fuction that print if integer is posive or negative
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i=0;
+	positive_or_negative(i);
+
+	return (0);
+}
