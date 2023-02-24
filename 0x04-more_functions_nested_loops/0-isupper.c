b1d90cbf36eb750cc296648ece625edc9b749f8b
tuyiz <honore.tuyizere73@gmail.com>
2023-02-24 11:42:17 -0800

    1-isdigit.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,13 +1,13 @@
-#include<main.h>
+#include"main.h"
 /**
  * _isupper - check if letter is upper case
- * @c: The number to be used
+ * @x: The number to be used
  * Return: 1 for upper letter and 0 for any alse
  */
 
-int _isupper(int c)
+int _isupper(int x)
 {
-	if (c >= 65 && c <= 90)
+	if (x >=  65 && x <= 90)
 	{
 	return (1);
 	}
---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include"main.h"
+/**
+ * _isdigit - ckech if charactor is a digit
+ * @c: The charactor to be used
+ * Return: 1 for charactor that will be digit or 0 for any elsee
+ */
+int _isdigit (int c)
+{
+	if (c >= 48 && c <= 56)
+	{
+	return (1);
+	}
+	return (0);
+}
