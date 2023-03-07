39555eed49e4e5d3e5aa0dbc328e415499512ffa
tuyiz <honore.tuyizere73@gmail.com>
2023-03-07 08:02:18 -0800

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+/**
+ * _memset - Fills momory with consant byte.
+ * @s: The storage address of memory to be filled.
+ * @b: The desired value
+ * @n: number of bytes to be changed
+ * Return: changed array with new value for n bytes
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return(s);
+}
